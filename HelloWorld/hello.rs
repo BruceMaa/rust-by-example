958ae16930e3ff7874a1228d35dcdc9f7f367fbc
BruceMaa <bruce.maqiang@gmail.com>
2019-11-15 15:23:08 +0800

    Update hello.rs
    添加注释

--- HelloWorld/hello.rs
+++ HelloWorld/hello.rs
@@ -1,4 +1,6 @@
 fn main() {
-    println!("Hello World!");
-    println!("I`m a Rustacean!");
+    // println!("Hello World!");
+    // println!("I`m a Rustacean!");
+    let x = 5 + /* 90 + */5;
+    println!("Is `x` 10 or 100? x = {}", x);
 }
