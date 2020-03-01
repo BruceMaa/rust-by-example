ccc0d4418ae229d757945a7fc71132483742e3ba
BruceMaa <bruce.maqiang@gmail.com>
2020-03-01 18:37:47 +0800

    Create rary.rs
    库

---
+++ cates/rary.rs
@@ -0,0 +1,14 @@
+
+pub fn public_funcion() {
+    println!("called rary's `public_funcion()`");
+}
+
+fn private_function() {
+    println!("called rary's `private_function()`");
+}
+
+pub fn indirect_access() {
+    print!("called rary's `indirect_access()`, that\n> ");
+
+    private_function();
+}
