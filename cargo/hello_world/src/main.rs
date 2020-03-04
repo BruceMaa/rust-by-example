e8237bf55dd80fb8d5d1e1c8f7f1e93fad13364d
BruceMaa <bruce.maqiang@gmail.com>
2020-03-04 10:10:34 +0800

    Update main.rs
    cargo dependencies

--- cargo/hello_world/src/main.rs
+++ cargo/hello_world/src/main.rs
@@ -1,3 +1,6 @@
+use regex::Regex;
+
 fn main() {
-    println!("Hello, world!");
+    let re = Regex::new(r"^\d{4}-\d{2}-\d{2}$").unwrap();
+    println!("Did out date match? {}", re.is_match("2014-01-01"));
 }
